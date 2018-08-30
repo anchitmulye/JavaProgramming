e3d881b6b8f4b269fd81da6f99e5fd2ce0d16258
Anchit Mulye <31447977+anchitmulye@users.noreply.github.com>
2018-08-30 20:56:49 +0530

    Add files via upload
    Java Begins!

---
+++ Introduction/Variables.java
@@ -0,0 +1,11 @@
+class Variables{
+    public static void main(String args[]){
+        int a = 5;
+        String b = "Five";
+        String space = " ";
+        char i = 'i';
+        char s = 's';
+        char stop = '.';        
+        System.out.println(a + space + i + s + space + b + stop);
+    }
+}
